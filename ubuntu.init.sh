e6dadf4cc2f223196f61bd7568d80598b92ecfdf
zsp042 <zsp042@gmail.com>
2020-07-31 16:31:03 +0800

    master

--- ubuntu.init.sh
+++ ubuntu.init.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# bash <(curl -s )
-
+# bash <(curl -sL http://t.cn/A6UyO55v)
 
 sudo mkdir -p /root/.ssh
 sudo cp ~/.ssh/authorized_keys /root/.ssh/authorized_keys
 sudo chown -R root:root /root/.ssh
 sudo apt-get update
+curl -fsSL https://get.docker.com | sudo bash -s docker --mirror Aliyun
+sudo systemctl start docker
+sudo systemctl enable docker
 
